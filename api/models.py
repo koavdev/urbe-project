8aeaa6decaaa72033fc084a9de9a9f368d100a0f
koavdev <koavdev@gmail.com>
2023-08-27 06:15:18 -0300

    feat: create models and set up db

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+
+    # texto das notas
+    body = models.TextField(null=True, blank=True)
+
+    # data da atualização das notas
+    updated = models.DateTimeField(auto_now=True)
+
+    # data da criação das notas
+    created = models.DateTimeField(auto_now_add=True)
+
+    # retorna apenas os primeiros 50 caracteres do corpo das notas 
+    def __str__(self):
+        return self.body[0:50]
