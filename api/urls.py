cf092b1ded48f6e556c5853a522f7b0fd752a0fa
koavdev <koavdev@gmail.com>
2023-08-27 18:01:41 -0300

    feat: add update note view

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="notes"),
-    path('notes/<str:pk>/', views.getNote, name="note")
+    path('notes/<str:pk>/update/', views.updateNote, name="update"),
+    path('notes/<str:pk>/', views.getNote, name="note"),
+
 ]
