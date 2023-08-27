3ddb63c0a0638e4345ffca1a390a106d3d8aabd4
koavdev <koavdev@gmail.com>
2023-08-27 16:33:36 -0300

    feat: add /note/id route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import Header from './components/Header';
 import './App.css';
 import NotesListPage from './pages/NotesListPage';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NotePage from './pages/NotePage'
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage />
-      My App
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path='/' exact component={NotesListPage} />
+        <Route path='/note/:id' component={NotePage} />
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
 
@@ -19,7 +20,7 @@ const NotesListPage = () => {
     <div>
       <div className='notes-list'>
         {notes.map((note, index) => (
-          <h3>{note.body}</h3>
+          <ListItem key={index} note={note} />
         ))}
       </div>
     </div>
